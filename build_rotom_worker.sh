b083410eb1102924b5905fc05115a9be8bfe393d
Wellington Antonio Borges <wellington1092@gmail.com>
2025-10-22 11:40:47 -0300

    Update build_rotom_worker.sh

--- build_rotom_worker.sh
+++ build_rotom_worker.sh
@@ -32,20 +32,24 @@ $CC -std=c++17 --sysroot=$TOOLCHAIN/sysroot \
 
 # Linka binário final
 echo "[+] Linkando rotom_worker_arm64..."
-echo "[+] Linkando rotom_worker_arm64..."
 $CC -o rotom_worker_arm64 main.o rotom.pb.o \
   -L$PROTOBUF_LIB \
   -L$ABSL_LIB/base -L$ABSL_LIB/container -L$ABSL_LIB/hash \
   -L$ABSL_LIB/log -L$ABSL_LIB/synchronization -L$ABSL_LIB/strings -L$ABSL_LIB/types -L$ABSL_LIB/utility \
+  -L$ABSL_LIB/status -L$ABSL_LIB/time -L$ABSL_LIB/debugging \
   -l:libprotobuf.a \
-  -labsl_base -labsl_malloc_internal -labsl_synchronization -labsl_strings \
+  -labsl_base -labsl_malloc_internal -labsl_synchronization -labsl_strings -labsl_strings_internal \
   -labsl_raw_hash_set -labsl_hash \
+  -labsl_cord -labsl_cord_internal -labsl_cordz_functions -labsl_cordz_info -labsl_cordz_sample_token \
+  -labsl_status -labsl_statusor \
+  -labsl_time -labsl_time_zone \
+  -labsl_str_format_internal -labsl_strerror \
+  -labsl_debugging_internal -labsl_stacktrace -labsl_symbolize \
   -labsl_log_internal_message -labsl_log_internal_check_op -labsl_log_internal_conditions \
   -labsl_log_internal_format -labsl_log_internal_globals -labsl_log_internal_proto \
   -labsl_log_internal_nullguard -labsl_log_initialize -labsl_log_severity \
   -llog -landroid -ldl -lm -pthread \
   -static-libstdc++ -static-libgcc
-
 
 
 echo "[✓] Build concluído com sucesso: rotom_worker_arm64"
